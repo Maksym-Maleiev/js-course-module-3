c08e1ef7d37b10bb3ddb3c3d9b6f06ea2d17de4f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-26 11:19:39 +0200

    Update 03-test.js

--- src/js/03-test.js
+++ src/js/03-test.js
@@ -1,3 +1,4 @@
+/*
 const book = {
   title: 'The Last Kingdom',
   author: 'Bernard Cornwell',
@@ -16,3 +17,19 @@ console.log(bookGenres);
 const bookPrice = book.price;
 console.log(bookPrice);
 // undefined
+*/
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// Change code below this line
+const aptRating = apartment.rating;
+const aptDescr = apartment.descr;
+const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+// Change code above this line
