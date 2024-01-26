7e0adad992110c74606a11e160323df2b4c1eb17
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-26 11:24:26 +0200

    Update 04-test.js

---
+++ src/js/04-test.js
@@ -0,0 +1,50 @@
+/*
+const user = {
+  name: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: { country: 'Jamaica', city: 'Ocho Rios' },
+  hobbies: ['swiming', 'music', 'sci-fi'],
+};
+
+const location = user.location;
+console.log(location);
+// Об'єкт location
+
+const country = user.location.country;
+console.log(country);
+// "Jamaica"
+
+const hobbies = user.hobbies;
+console.log(hobbies);
+// ["swiming", "music", "sci-fi"]
+
+const firstHobby = user.hobbies[0];
+console.log(firstHobby);
+// "swiming"
+
+const numberOfHobbies = user.hobbies.length;
+console.log(numberOfHobbies);
+// 3
+*/
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+// Change code below this line
+const ownerName = apartment;
+const ownerPhone = apartment;
+const ownerEmail = apartment;
+const numberOfTags = apartment;
+const firstTag = apartment;
+const lastTag = apartment;
+// Change code above this line
