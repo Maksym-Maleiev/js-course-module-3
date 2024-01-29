ff4cf141536a6d3f4b7be29534d8cf1ce95f9121
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-29 13:06:26 +0200

    Create 10-test.js

---
+++ src/js/10-test.js
@@ -0,0 +1,23 @@
+/*
+const book = {
+  title: 'The Last Kingdom',
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  rating: 8.38,
+};
+for (const key in book) {
+  // Ключ
+  console.log(key);
+  // Значення якості з таким ключем
+  console.log(book[key]);
+}
+*/
+
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
