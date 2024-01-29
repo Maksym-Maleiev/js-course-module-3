666d97576f4706a4ab8bb74760e273341f5f048f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-29 12:52:46 +0200

    Update 08-test.js

---
+++ src/js/08-test.js
@@ -0,0 +1,18 @@
+/*
+const name = 'Harry Potter';
+const age = 25;
+const user = { name: name, age: age };
+console.log(user.name);
+// "Harry Potter"
+console.log(user.age);
+// 25
+*/
+
+const name = 'Harry Potter';
+const age = 25;
+const user = { name, age };
+
+console.log(user.name);
+// "Harry Potter"
+console.log(user.age);
+// 25
