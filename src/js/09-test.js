b91d7b11fb780032b17685ebd253ffa1b1dbfdee
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-29 13:03:28 +0200

    Update 09-test.js

--- src/js/09-test.js
+++ src/js/09-test.js
@@ -24,5 +24,10 @@ const passwordInputName = 'password';
 
 const credentials = {
   // Change code below this line
+  [emailInputName]: 'henry.carter@aptmail.com',
+  [passwordInputName]: 'jqueryismyjam',
   // Change code above this line
 };
+
+console.log(credentials.email);
+console.log(credentials.password);
