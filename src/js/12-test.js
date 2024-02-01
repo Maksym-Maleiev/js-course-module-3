0c667deba215eeeae0a2c126566348d0ba2f9f2c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-01 12:36:51 +0200

    Update 12-test.js

--- src/js/12-test.js
+++ src/js/12-test.js
@@ -1,7 +1,11 @@
 function countProps(object) {
   let propCount = 0;
   // Change code below this line
-
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      propCount += 1;
+    }
+  }
   // Change code above this line
   return propCount;
 }
