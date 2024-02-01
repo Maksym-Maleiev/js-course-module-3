69ccbd9e94fe8057ef5e35b96841b4bb86d70896
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-01 15:03:20 +0200

    Update 13-test.js

--- src/js/13-test.js
+++ src/js/13-test.js
@@ -1,3 +1,4 @@
+/*
 const book = {
   title: 'The Last Kingdom',
   author: 'Bernard Cornwell',
@@ -7,3 +8,18 @@ const book = {
 const keys = Object.keys(book);
 console.log(keys);
 // ['title', 'author', 'genres', 'rating']
+*/
+
+const book = {
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  rating: 8.38,
+};
+
+const keys = Object.keys(book);
+for (const key of keys) {
+  // Ключ
+  console.log(key);
+  // Значення властивості
+  console.log(book[key]);
+}
