79ff51c90f70d0999868d5dcc2e04460f98dc2d5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-01 15:01:19 +0200

    Update 13-test.js

---
+++ src/js/13-test.js
@@ -0,0 +1,9 @@
+const book = {
+  title: 'The Last Kingdom',
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+console.log(keys);
+// ['title', 'author', 'genres', 'rating']
