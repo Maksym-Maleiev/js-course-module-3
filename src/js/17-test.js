d6e13e7c858a9188986e9f03d263c55d865d723d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 14:28:59 +0200

    Update 17-test.js

--- src/js/17-test.js
+++ src/js/17-test.js
@@ -1,3 +1,4 @@
+/*
 const books = [
   { title: 'The Last Kingdom', author: 'Bernard Cornwell', rating: 8.38 },
   { title: 'Beside Still Waters', author: 'Robert Sheckley', rating: 8.51 },
@@ -18,3 +19,23 @@ for (const book of books) {
   // Рейтинг
   console.log(book.rating);
 }
+*/
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+console.log(hexColors);
+console.log(rgbColors);
