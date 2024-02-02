ab2165138167416e01e72e0067fa35fd0f913642
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 14:22:08 +0200

    Update 16-test.js

--- src/js/16-test.js
+++ src/js/16-test.js
@@ -1,7 +1,11 @@
 function countTotalSalary(salaries) {
   let totalSalary = 0;
   // Change code below this line
+  const values = Object.values(salaries);
 
+  for (const value of values) {
+    totalSalary += value;
+  }
   // Change code above this line
   return totalSalary;
 }
