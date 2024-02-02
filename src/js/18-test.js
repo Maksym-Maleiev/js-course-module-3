56ebe44b03a6b7b120d482d176d3778988fb15d9
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 14:32:28 +0200

    Update 18-test.js

--- src/js/18-test.js
+++ src/js/18-test.js
@@ -7,5 +7,18 @@ const products = [
 
 function getProductPrice(productName) {
   // Change code below this line
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price;
+    }
+  }
+
+  return null;
   // Change code above this line
 }
+
+console.log(getProductPrice('Radar'));
+console.log(getProductPrice('Grip'));
+console.log(getProductPrice('Scanner'));
+console.log(getProductPrice('Droid'));
+console.log(getProductPrice('Engine'));
