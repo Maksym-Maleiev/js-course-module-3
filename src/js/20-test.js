5846bf1d9b07145fa7c7b0f508af0971fecd19ed
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 14:53:10 +0200

    Update 20-test.js

---
+++ src/js/20-test.js
@@ -0,0 +1,11 @@
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код нижче за цей рядок
+  // Пиши код вище за цей рядок
+}
