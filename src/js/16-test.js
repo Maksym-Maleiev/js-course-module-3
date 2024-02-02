83c8e5fbc700a5efe413e7ef2316847e8b0acda0
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 14:18:19 +0200

    Update 16-test.js

--- src/js/16-test.js
+++ src/js/16-test.js
@@ -5,3 +5,7 @@ function countTotalSalary(salaries) {
   // Change code above this line
   return totalSalary;
 }
+
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
