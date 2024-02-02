94ee3f22f693365098d24a1229dba95479215282
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-02 15:04:11 +0200

    Update 20-test.js

--- src/js/20-test.js
+++ src/js/20-test.js
@@ -7,5 +7,20 @@ const products = [
 
 function calculateTotalPrice(productName) {
   // Пиши код нижче за цей рядок
+  let totalPrice = 0;
+
+  for (const product of products) {
+    if (product[productName] === product.name) {
+      return (totalPrice = product.price * product.quantity);
+    }
+  }
+
+  return totalPrice;
   // Пиши код вище за цей рядок
 }
+
+console.log(calculateTotalPrice('Blaster'));
+console.log(calculateTotalPrice('Radar'));
+console.log(calculateTotalPrice('Droid'));
+console.log(calculateTotalPrice('Grip'));
+console.log(calculateTotalPrice('Scanner'));
