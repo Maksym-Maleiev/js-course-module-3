29ba9a79ca21a7988b74fd0c405580bb9849d5a9
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-05 12:31:35 +0200

    Update 22-test.js

--- src/js/22-test.js
+++ src/js/22-test.js
@@ -1,3 +1,4 @@
+/*
 const book = { title: 'The Last Kingdom', author: 'Bernard Cornwell' };
 
 // Додамо картинку обкладинки якщо її немає в об'єкті книги
@@ -13,3 +14,19 @@ console.log(author);
 // "Bernard Cornwell"
 console.log(coverImage);
 // "https://via.placeholder.com/640/480"
+*/
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const yesterday = highTemperatures.yesterday;
+const today = highTemperatures.today;
+const tomorrow = highTemperatures.tomorrow;
+const icon = highTemperatures.icon;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
