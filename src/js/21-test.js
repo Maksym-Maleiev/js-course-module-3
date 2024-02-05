cd6273d25f01225bf719a9c0e434ad2b93f3d83d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-05 12:21:12 +0200

    Update 21-test.js

---
+++ src/js/21-test.js
@@ -0,0 +1,9 @@
+const book = {
+  title: 'The Last Kingdom',
+  автор: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  public: true,
+  rating: 8.38,
+};
+const accessType = book.public ? 'публічному' : 'закритому';
+const message = `Книга ${book.title} автора ${book.author} з рейтингом ${book.rating} знаходиться у ${accessType} доступі.`;
