4ddac481d8c263c85ebb0bf72fe9ea9dbbc2c92a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-09 13:57:26 +0200

    Update 24-test.js

--- src/js/24-test.js
+++ src/js/24-test.js
@@ -2,6 +2,7 @@ const books = [
   { title: 'The Last Kingdom', author: 'Bernard Cornwell', rating: 8.38 },
   { title: 'Beside Still Waters', author: 'Robert Sheckley', rating: 8.51 },
 ];
+
 for (const book of books) {
   console.log(book.title);
   console.log(book.author);
