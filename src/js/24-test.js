99a6bc184586f3becc6dbb303d9be941c8e76f26
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-09 13:58:27 +0200

    Update 24-test.js

--- src/js/24-test.js
+++ src/js/24-test.js
@@ -3,8 +3,21 @@ const books = [
   { title: 'Beside Still Waters', author: 'Robert Sheckley', rating: 8.51 },
 ];
 
-for (const book of books) {
-  console.log(book.title);
-  console.log(book.author);
-  console.log(book.rating);
+// for (const book of books) {
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+// }
+
+// for (const book of books) {
+//   const { title, author, rating } = book;
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+
+for (const { title, author, rating } of books) {
+  console.log(title);
+  console.log(author);
+  console.log(rating);
 }
