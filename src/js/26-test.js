7503aa1226e8bec70e9453050f62af7e0ff98e1a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-09 19:00:39 +0200

    Update 26-test.js

--- src/js/26-test.js
+++ src/js/26-test.js
@@ -57,12 +57,12 @@ function doStuffWithBook({
 */
 
 // Change code below this line
-function calculateMeanTemperature(forecast) {
-  const todayLow = forecast.today.low;
-  const todayHigh = forecast.today.high;
-  const tomorrowLow = forecast.tomorrow.low;
-  const tomorrowHigh = forecast.tomorrow.high;
-
+function calculateMeanTemperature({
+  todayLow,
+  todayHigh,
+  tomorrowLow,
+  tomorrowHigh,
+}) {
   // Change code above this line
   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
 }
