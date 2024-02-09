ef1866b992e7a0c617d63639eb6b47ee343a6acb
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-09 18:43:12 +0200

    Update 25-test.js

--- src/js/25-test.js
+++ src/js/25-test.js
@@ -36,14 +36,6 @@ const forecast = {
 };
 // Change code below this line
 
-// const highToday = forecast.today.high;
-// const lowToday = forecast.today.low;
-// const todayIcon = forecast.today.icon;
-
-// const highTomorrow = forecast.tomorrow.high;
-// const lowTomorrow = forecast.tomorrow.low;
-// const tomorrowIcon = forecast.tomorrow.icon;
-
 const {
   today: {
     low: lowToday,
