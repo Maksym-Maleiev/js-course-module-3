927884cf659ea0e20ae651ed06b01016bd7daf85
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 18:30:13 +0200

    Create 40-test.js

---
+++ src/js/40-test.js
@@ -0,0 +1,7 @@
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  updatePotionName(oldName, newName) {
+    // Change code below this line
+    // Change code above this line
+  },
+};
