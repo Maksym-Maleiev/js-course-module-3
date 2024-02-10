c66ea8fd3feb6487d8ae775fd64b26cb8cec77e8
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 13:43:24 +0200

    Update 30-test.js

--- src/js/30-test.js
+++ src/js/30-test.js
@@ -3,6 +3,29 @@ function makeTask(data) {
   const category = 'General';
   const priority = 'Normal';
   // Change code below this line
+  const newObject = {
+    category: category,
+    priority: priority,
+    completed: completed,
+    ...data,
+  };
 
+  return newObject;
   // Change code above this line
 }
+
+console.log(makeTask({}));
+
+console.log(
+  makeTask({
+    category: 'Homemade',
+    priority: 'Low',
+    text: 'Take out the trash',
+  })
+);
+
+console.log(makeTask({ category: 'Finance', text: 'Take interest' }));
+
+console.log(makeTask({ priority: 'Low', text: 'Choose shampoo' }));
+
+console.log(makeTask({ text: 'Buy bread' }));
