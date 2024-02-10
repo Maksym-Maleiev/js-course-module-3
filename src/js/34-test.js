e871922f447bffdd5b000e24c6377190670256fe
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 15:00:47 +0200

    Update 34-test.js

--- src/js/34-test.js
+++ src/js/34-test.js
@@ -23,3 +23,20 @@ const books = [];
 function getBooks() {}
 function addBook() {}
 */
+
+const bookShelf = {
+  // Change code below this line
+  books: ['The last kingdom', 'The guardian of dreams'],
+  getBooks() {
+    return 'Returning all books';
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  // Change code above this line
+};
+
+console.log(bookShelf.getBooks());
+console.log(bookShelf.addBook('Haze'));
+console.log(bookShelf.removeBook('Red sunset'));
+console.log(bookShelf.updateBook('Sands of dune', 'Dune'));
