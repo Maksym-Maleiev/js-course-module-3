74d0d7c3bcc42d3f505146af8c329e6878deb864
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 13:50:32 +0200

    Update 31-test.js

--- src/js/31-test.js
+++ src/js/31-test.js
@@ -1,7 +1,18 @@
+/*
 // Як оголосити параметри функції так,
 // щоб можна було передати будь-яку кількість аргументів?
 function multiply() {
   // ...
+}
+
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
+*/
+
+function multiply(...args) {
+  console.log(args);
+  // масив всіх аргументів
 }
 
 multiply(1, 2);
