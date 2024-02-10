9b1425eaeb4ee8e002821bb30a018c82d44e1600
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 13:17:43 +0200

    Update 28-test.js

--- src/js/28-test.js
+++ src/js/28-test.js
@@ -1,5 +1,14 @@
+/*
 const temps = [14, -4, 25, 8, 11];
 // Це точна, але незалежна копія масиву temps
 const copyOfTemps = [...temps];
 console.log(copyOfTemps);
 // [14, -4, 25, 8, 11]
+*/
+
+const lastWeekTemps = [14, 25, 11];
+const currentWeekTemps = [23, 17, 18];
+const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+
+console.log(allTemps);
+// [14, 25, 11, 23, 17, 18]
