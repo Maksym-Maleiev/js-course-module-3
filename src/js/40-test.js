cda4a90767c515b277e982164476960468434e90
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 18:32:05 +0200

    Update 40-test.js

--- src/js/40-test.js
+++ src/js/40-test.js
@@ -2,6 +2,10 @@ const atTheOldToad = {
   potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
   updatePotionName(oldName, newName) {
     // Change code below this line
+    const indexPotion = this.potions.indexOf(oldName);
+
+    this.potions.splice(indexPotion, 1, newName);
+
     // Change code above this line
   },
 };
