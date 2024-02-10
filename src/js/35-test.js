e97d4de34c3802d01ae767f635eb10285cc2577c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 16:29:21 +0200

    Create 35-test.js

---
+++ src/js/35-test.js
@@ -0,0 +1,12 @@
+/*
+const bookShelf = {
+  books: ['Останнє королівство'],
+  getBooks() {
+    console.log(this);
+  },
+};
+// Перед точкою стоїть об'єкт bookShelf,
+// тому при виклику методу це буде зберігати посилання на нього.
+bookShelf.getBooks();
+// {books: ["Остання королівство"], getBooks: f}
+*/
