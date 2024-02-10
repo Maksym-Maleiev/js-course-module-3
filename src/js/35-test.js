59648328cd7ef483d0f524c8e837e87985e5d42f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 16:52:42 +0200

    Update 35-test.js

--- src/js/35-test.js
+++ src/js/35-test.js
@@ -10,3 +10,27 @@ const bookShelf = {
 bookShelf.getBooks();
 // {books: ["Остання королівство"], getBooks: f}
 */
+
+const bookShelf = {
+  books: ['Останнє королівство'],
+  getBooks() {
+    return this.books;
+  },
+  addBook(bookName) {
+    this.books.push(bookName);
+  },
+  removeBook(bookName) {
+    const bookIndex = this.books.indexOf(bookName);
+    this.books.splice(bookIndex, 1);
+  },
+};
+
+console.log(bookShelf.getBooks());
+// ["Остання королівство"]
+bookShelf.addBook('Мгла');
+bookShelf.addBook('Вартовий снів');
+console.log(bookShelf.getBooks());
+// ["Остання королівство", "Мгла", "Вартовий снів"]
+bookShelf.removeBook('Мгла');
+console.log(bookShelf.getBooks());
+// ["Остання королівство", "Вартовий снів"]
