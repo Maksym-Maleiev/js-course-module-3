7c36498a1e970d2e07d0ea41828b7bf3c09da78b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 14:34:55 +0200

    Update 32-test.js

--- src/js/32-test.js
+++ src/js/32-test.js
@@ -18,9 +18,16 @@ function addOverNum(firstArg, ...args) {
   let total = 0;
 
   for (const arg of args) {
-    if (arg > firstArg) total += arg;
+    if (arg > firstArg) {
+      total += arg;
+    }
   }
 
   return total;
   // Change code above this line
 }
+
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
