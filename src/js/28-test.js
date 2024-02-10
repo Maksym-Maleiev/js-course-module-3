2dfbf870907b2fe6430b4dea1077237e3e7f97b3
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 13:18:27 +0200

    Update 28-test.js

--- src/js/28-test.js
+++ src/js/28-test.js
@@ -6,9 +6,19 @@ console.log(copyOfTemps);
 // [14, -4, 25, 8, 11]
 */
 
+/*
 const lastWeekTemps = [14, 25, 11];
 const currentWeekTemps = [23, 17, 18];
 const allTemps = [...lastWeekTemps, ...currentWeekTemps];
 
 console.log(allTemps);
 // [14, 25, 11, 23, 17, 18]
+*/
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [];
+const bestScore = allScores;
+const worstScore = allScores;
