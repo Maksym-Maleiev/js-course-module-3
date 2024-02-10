360e2b65b68bfafff50a8b2cbfd115b3e51ff1dd
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 14:47:26 +0200

    Update 33-test.js

--- src/js/33-test.js
+++ src/js/33-test.js
@@ -1,7 +1,20 @@
 // Change code below this line
-function findMatches() {
+function findMatches(arr, ...arges) {
   const matches = []; // Don't change this line
+
+  for (const arg of arges) {
+    for (const element of arr) {
+      if (element === arg) {
+        matches.push(element);
+      }
+    }
+  }
 
   // Change code above this line
   return matches;
 }
+
+console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
+console.log(findMatches([63, 11, 8, 29], 4, 7, 16));
