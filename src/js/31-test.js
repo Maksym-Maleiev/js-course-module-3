695defa6f1a1ee281c4130a831691ef13daeac7b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 14:09:00 +0200

    Update 31-test.js

--- src/js/31-test.js
+++ src/js/31-test.js
@@ -23,6 +23,14 @@ multiply(1, 2, 3, 4);
 
 // Change code below this line
 function add(...args) {
-  let sum = ;
+  console.log(...args);
+  let sum = 0;
+
+  return sum;
   // Change code above this line
 }
+
+console.log(add(15, 27));
+console.log(add(12, 4, 11, 48));
+console.log(add(32, 6, 13, 19, 8));
+console.log(add(74, 11, 62, 46, 12, 36));
