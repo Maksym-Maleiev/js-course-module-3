0be303ed115a8c169c865abf1a1b973cc4cdc2f9
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 18:28:06 +0200

    Update 39-test.js

--- src/js/39-test.js
+++ src/js/39-test.js
@@ -2,6 +2,14 @@ const atTheOldToad = {
   potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
   removePotion(potionName) {
     // Change code below this line
+    const indexPotion = this.potions.indexOf(potionName);
+
+    this.potions.splice(indexPotion, 1);
+
+    return this.potions;
     // Change code above this line
   },
 };
+
+console.log(atTheOldToad.removePotion('Dragon breath'));
+console.log(atTheOldToad.removePotion('Speed potion'));
