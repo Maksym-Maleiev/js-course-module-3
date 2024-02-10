e6df3db08cd166f86b1449fcde91f143f5e7aba7
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-10 13:29:00 +0200

    Update 29-test.js

--- src/js/29-test.js
+++ src/js/29-test.js
@@ -6,6 +6,7 @@ console.log(third);
 // { propA: 5, propB: 10, propC: 15 }
 */
 
+/*
 const first = { propA: 5, propB: 10, propC: 50 };
 const second = { propC: 15, propD: 20 };
 const third = { ...first, ...second };
@@ -14,3 +15,16 @@ console.log(third);
 const fourth = { ...second, ...first };
 console.log(fourth);
 // { propA: 5, propB: 10, propC: 50, propD: 20 }
+*/
+
+const first = { propA: 5, propB: 10, propC: 50 };
+const second = { propC: 15 };
+const third = { propB: 20, ...first, ...second };
+console.log(third);
+// { propA: 5, propB: 10, propC: 15 }
+const fourth = { ...first, ...second, propB: 20 };
+console.log(fourth);
+// { propA: 5, propB: 20, propC: 15 }
+const fifth = { ...first, propB: 20, ...second };
+console.log(fifth);
+// { propA: 5, propB: 20, propC: 15 }
